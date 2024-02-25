8bca672108ed4410ae07ac55ff6a9b18e74b4f77
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-02-25 12:03:32 +0200

    _v, updateStatus fixed

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -75,14 +75,14 @@ export const updateContact = async (req, res, next) => {
 export const updateStatusContact = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const { favorite } = req.body;
-    const result = await Contact.findByIdAndUpdate(id, { favorite }, { new: true });
+
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
 
     if (!result) {
       throw HttpError(404, `Not found`);
     }
 
-    res.status(200).json(updateStatus);
+    res.status(200).json(result);
   } catch (error) {
     next(error);
   }
--- model/contact-model.js
+++ model/contact-model.js
@@ -1,22 +1,25 @@
 import { model, Schema } from 'mongoose';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false },
+);
 
 const Contact = model('Contact', contactSchema);
 
